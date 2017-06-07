dacab78d8ce2b0b1df51f559a933e12b413d9704
xiaokecong <xiaokecong@baidu.com>
2017-06-07 16:21:13 +0800

    使用MediaCodec, MediaExtractor, MediaMuxer对已有视频进行剪切

---
+++ app/src/main/java/com/android/cong/mediaeditdemo/videocut/VideoClipActivity.java
@@ -0,0 +1,34 @@
+package com.android.cong.mediaeditdemo.videocut;
+
+import com.android.cong.mediaeditdemo.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by xiaokecong on 07/06/2017.
+ */
+
+public class VideoClipActivity extends Activity {
+    private Button btnClip;
+
+    private final String filePath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM)
+            .getAbsolutePath() + "/Video/V70602-103441.mp4";
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_clip);
+
+        btnClip = (Button) findViewById(R.id.btn_clip);
+        btnClip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new VideoDecoder().decodeVideo(filePath, 0, 3000000);
+            }
+        });
+    }
+}
