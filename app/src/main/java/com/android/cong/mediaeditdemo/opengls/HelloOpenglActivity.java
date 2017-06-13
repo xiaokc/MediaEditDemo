c93e34079e31d4f21bbb3b00179afa4690c25dba
xiaokecong <xiaokecong@baidu.com>
2017-06-13 16:41:58 +0800

    添加投影和相机视图

--- app/src/main/java/com/android/cong/mediaeditdemo/opengls/HelloOpenglActivity.java
+++ app/src/main/java/com/android/cong/mediaeditdemo/opengls/HelloOpenglActivity.java
@@ -10,8 +10,10 @@ import javax.microedition.khronos.opengles.GL10;
 import com.android.cong.mediaeditdemo.R;
 
 import android.app.Activity;
+
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
+import android.opengl.Matrix;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
@@ -33,7 +35,9 @@ public class HelloOpenglActivity extends Activity {
     }
 
     private static class MyRenderer implements GLSurfaceView.Renderer {
-        private static final String VERTEX_SHADER = "attribute vec4 vPosition; void main(){gl_Position=vPosition;}";
+        private static final String VERTEX_SHADER = "attribute vec4 vPosition; "
+                + "uniform mat4 uMVPMatrix;"
+                + "void main(){gl_Position=uMVPMatrix * vPosition;}";
         private static final String FRAGMENT_SHADER = "precision mediump float; "
                 + "void main(){gl_FragColor=vec4(0.5,0,0,1);}";
         private static final float[] VERTEX = { // 逆时针方向
@@ -43,8 +47,12 @@ public class HelloOpenglActivity extends Activity {
         };
 
         private final FloatBuffer mVertexBuffer;
+        private final float[] mProjectionMatrix = new float[16]; // 投影矩阵
+        private final float[] mCameraMatrix = new float[16]; // 相机矩阵
+        private final float[] mMVPMatrix = new float[16];
         private int mProgram;
         private int mPositionHandler;
+        private int mMatrixHandler;
 
         MyRenderer() {
             mVertexBuffer = ByteBuffer.allocateDirect(VERTEX.length * 4)
@@ -69,6 +77,12 @@ public class HelloOpenglActivity extends Activity {
             GLES20.glLinkProgram(mProgram);
 
             mPositionHandler = GLES20.glGetAttribLocation(mProgram, "vPosition");
+            mMatrixHandler = GLES20.glGetUniformLocation(mProgram, "uMVPMatrix");
+
+            float ratio = (float) height / width;
+            Matrix.frustumM(mProjectionMatrix, 0, -1, 1, -ratio, ratio, 3, 7);
+            Matrix.setLookAtM(mCameraMatrix, 0, 0, 0, 3, 0, 0, 0, 1, 0, 0);
+            Matrix.multiplyMM(mMVPMatrix, 0, mProjectionMatrix, 0, mCameraMatrix, 0);
 
         }
 
@@ -78,6 +92,8 @@ public class HelloOpenglActivity extends Activity {
             GLES20.glUseProgram(mProgram);
             GLES20.glEnableVertexAttribArray(mPositionHandler);
             GLES20.glVertexAttribPointer(mPositionHandler, 3, GLES20.GL_FLOAT, false, 12, mVertexBuffer);
+
+            GLES20.glUniformMatrix4fv(mMatrixHandler, 1, false, mMVPMatrix, 0);
 
             GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, 3);
             GLES20.glDisableVertexAttribArray(mPositionHandler);
