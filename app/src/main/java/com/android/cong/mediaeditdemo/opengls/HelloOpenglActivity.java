90c50e64adeef2b398149b3220295cbddd90f2eb
xiaokecong <xiaokecong@baidu.com>
2017-06-13 15:58:47 +0800

    hello OpenGL ES
    使用OpenGL ES，在GLSurfaceView上画一个三角形

---
+++ app/src/main/java/com/android/cong/mediaeditdemo/opengls/HelloOpenglActivity.java
@@ -0,0 +1,93 @@
+package com.android.cong.mediaeditdemo.opengls;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import com.android.cong.mediaeditdemo.R;
+
+import android.app.Activity;
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by xiaokecong on 13/06/2017.
+ */
+
+public class HelloOpenglActivity extends Activity {
+    private GLSurfaceView glSurfaceView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello_opengl);
+        glSurfaceView = (GLSurfaceView) findViewById(R.id.gl_surfaceview);
+        glSurfaceView.setEGLContextClientVersion(2);
+        glSurfaceView.setRenderer(new MyRenderer());
+        glSurfaceView.setRenderMode(GLSurfaceView.RENDERMODE_CONTINUOUSLY);
+    }
+
+    private static class MyRenderer implements GLSurfaceView.Renderer {
+        private static final String VERTEX_SHADER = "attribute vec4 vPosition; void main(){gl_Position=vPosition;}";
+        private static final String FRAGMENT_SHADER = "precision mediump float; "
+                + "void main(){gl_FragColor=vec4(0.5,0,0,1);}";
+        private static final float[] VERTEX = { // 逆时针方向
+                0, 1, 0, // 顶部的点
+                -0.5f, -1, 0, // 左下角的点
+                1, -1, 0, // 右下角的点
+        };
+
+        private final FloatBuffer mVertexBuffer;
+        private int mProgram;
+        private int mPositionHandler;
+
+        MyRenderer() {
+            mVertexBuffer = ByteBuffer.allocateDirect(VERTEX.length * 4)
+                    .order(ByteOrder.nativeOrder())
+                    .asFloatBuffer()
+                    .put(VERTEX);
+            mVertexBuffer.position(0);
+        }
+
+        @Override
+        public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+
+        }
+
+        @Override
+        public void onSurfaceChanged(GL10 gl, int width, int height) {
+            mProgram = GLES20.glCreateProgram();
+            int vertexShader = loadShader(GLES20.GL_VERTEX_SHADER, VERTEX_SHADER);
+            int fragmentShader = loadShader(GLES20.GL_FRAGMENT_SHADER, FRAGMENT_SHADER);
+            GLES20.glAttachShader(mProgram, vertexShader);
+            GLES20.glAttachShader(mProgram, fragmentShader);
+            GLES20.glLinkProgram(mProgram);
+
+            mPositionHandler = GLES20.glGetAttribLocation(mProgram, "vPosition");
+
+        }
+
+        @Override
+        public void onDrawFrame(GL10 gl) {
+            GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
+            GLES20.glUseProgram(mProgram);
+            GLES20.glEnableVertexAttribArray(mPositionHandler);
+            GLES20.glVertexAttribPointer(mPositionHandler, 3, GLES20.GL_FLOAT, false, 12, mVertexBuffer);
+
+            GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, 3);
+            GLES20.glDisableVertexAttribArray(mPositionHandler);
+        }
+
+        static int loadShader(int type, String shaderCode) {
+            int shader = GLES20.glCreateShader(type);
+            GLES20.glShaderSource(shader, shaderCode);
+            GLES20.glCompileShader(shader);
+            return shader;
+        }
+    }
+}
